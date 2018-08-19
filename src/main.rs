08f6395d4ba11865ccf2dde9438f3b3382a3596f
Caleb Boylan <calebboylan@gmail.com>
2018-08-19 15:20:08 -0700

    Add tutorial 1 code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#[macro_use]
+extern crate glium;
+
+use glium::Surface;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::glutin;
+
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let mut closed = false;
+    while !closed {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+        events_loop.poll_events(|ev| {
+            match ev {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::CloseRequested => closed = true,
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
+    }
+
 }
