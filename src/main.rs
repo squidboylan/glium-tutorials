242805dcd78874e24af7193ed43a591dea545d60
Caleb Boylan <calebboylan@gmail.com>
2018-08-19 23:13:47 -0700

    Add tutorial 12 code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,40 @@ use glium::Surface;
 
 mod teapot;
 
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+}
+
 fn main() {
     use glium::glutin;
 
@@ -18,19 +52,21 @@ fn main() {
     let mut closed = false;
 
     let vertex_shader_src = r#"
-    #version 150
+    #version 140
 
     in vec3 position;
     in vec3 normal;
 
     out vec3 v_normal;
 
-    uniform mat4 perspective;       // new
-    uniform mat4 matrix;
+    uniform mat4 perspective;
+    uniform mat4 view;
+    uniform mat4 model;
 
     void main() {
-        v_normal = transpose(inverse(mat3(matrix))) * normal;
-        gl_Position = perspective * matrix * vec4(position, 1.0);       // new
+        mat4 modelview = view * model;
+        v_normal = transpose(inverse(mat3(modelview))) * normal;
+        gl_Position = perspective * modelview * vec4(position, 1.0);
     }
     "#;
 
@@ -67,6 +103,8 @@ fn main() {
         .. Default::default()
     };
 
+    let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+
     while !closed {
         let mut target = display.draw();
         let perspective = {
@@ -87,17 +125,17 @@ fn main() {
             ]
         };
 
-        let matrix = [
+        let model = [
                 [0.01, 0.0, 0.0, 0.0],
                 [0.0, 0.01, 0.0, 0.0],
                 [0.0, 0.0, 0.01, 0.0],
-                [0.0, 0.0, 5.0, 1.0f32],
+                [0.0, 0.0, 2.0, 1.0f32],
             ];
 
         target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
         target.draw((&positions, &normals), &indices, &program,
-                    &uniform! { matrix: matrix, perspective: perspective, u_light: light },
-                                &params).unwrap();
+            &uniform! { model: model, view: view, perspective: perspective, u_light: light },
+            &params).unwrap();
         target.finish().unwrap();
 
         events_loop.poll_events(|ev| {
